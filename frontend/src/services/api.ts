e28ba07366aabba6c418d1c830ce97a8cf56452f
KevinSoto07 <kevingranillo28@gmail.com>
2026-03-31 21:36:09 -0600

    update api url to production

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://task-manager-backend-dt8p.onrender.com',
 })
 
 //Interceptor: agrega el token automaticamente a cada petición
